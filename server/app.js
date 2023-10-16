2805ef98ca5e79a9936b3a6dcb9c441c549d0eb8
Juan Carlos Rios Lopez <124659889+juancarlosrslopez@users.noreply.github.com>
2023-10-16 03:57:53 +0000

    🌟 Feature: Domains

--- server/app.js
+++ server/app.js
@@ -1,35 +1,33 @@
-// Helps to handle http errors
-import createError from 'http-errors';
-// Import the Express Library
+// Cargando dependencias
 import express from 'express';
-// Is a Core-Node library to manage system paths
 import path from 'path';
-// Helps to parse client cookies
 import cookieParser from 'cookie-parser';
-// Library to log http communication
 import morgan from 'morgan';
 
 // Setting Webpack Modules
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
 import WebpackHotMiddleware from 'webpack-hot-middleware';
-// Importing webpack configuration
-import webpackConfig from '../webpack.dev.config';
 
 // Importing template-engine
 import configTemplateEngine from './config/templateEngine';
 
+// Importing webpack configuration
+import webpackConfig from '../webpack.dev.config';
+
 // Impornting winston logger
 import log from './config/winston';
 
-// Importando enrutador
+// Importing Router
 import router from './router';
+
+import debug from './services/debugLogger';
 
 // Creando variable del directorio raiz
 // eslint-disable-next-line
 global['__rootdir'] = path.resolve(process.cwd());
 
-// We are creating the express instance
+// Creando la instancia de express
 const app = express();
 
 // Get the execution mode
@@ -38,16 +36,19 @@ const nodeEnviroment = process.env.NODE_ENV || 'production';
 // Deciding if we add webpack middleware or not
 if (nodeEnviroment === 'development') {
   // Start Webpack dev server
-  console.log('🛠️  Ejecutando en modo desarrollo');
+  debug('🛠️ Ejecutando en modo desarrollo 🛠️');
   // Adding the key "mode" with its value "development"
   webpackConfig.mode = nodeEnviroment;
-  // Setting the port
+  // Setting the dev server port to the same value as the express server
   webpackConfig.devServer.port = process.env.PORT;
   // Setting up the HMR (Hot Module Replacement)
   webpackConfig.entry = [
     'webpack-hot-middleware/client?reload=true&timeout=1000',
     webpackConfig.entry,
   ];
+  // Agregar el plugin a la configuración de desarrollo
+  // de webpack
+  webpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
   // Creating the bundler
   const bundle = webpack(webpackConfig);
   // Enabling the webpack middleware
@@ -65,37 +66,15 @@ if (nodeEnviroment === 'development') {
 // Configuring the template engine
 configTemplateEngine(app);
 
-// Registering middlewares
-// Log all received requests
+// Se establecen los middlewares
 app.use(morgan('dev', { stream: log.stream }));
-// Parse request data into json
 app.use(express.json());
-// Decode url info
 app.use(express.urlencoded({ extended: false }));
-// Parse client cookies into json
 app.use(cookieParser());
-// Set up the static file server
-app.use(express.static(path.join(__dirname, '../public')));
+// Crea un server de archivos estaticos
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-// Registering routes
+// Registro de Rutas
 router.addRoutes(app);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  log.info(`404 Pagina no encontrada ${req.method} ${req.originalUrl}`);
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  log.error(`${err.status || 500} - ${err.message}`);
-  res.render('error');
-});
 
 export default app;
