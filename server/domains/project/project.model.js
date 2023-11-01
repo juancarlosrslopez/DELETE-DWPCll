9c203422ce50c3add2a7621f970070af0b787f17
Juan Carlos Rios Lopez <124659889+juancarlosrslopez@users.noreply.github.com>
2023-11-01 15:54:15 +0000

    ⭐ Feat: Compilando Modelo

---
+++ server/domains/project/project.model.js
@@ -0,0 +1,24 @@
+// Importando Mongoose
+import mongoose from 'mongoose';
+
+// Destructurando la función Schema
+const { Schema } = mongoose;
+
+// Construir un Schema
+const ProjectSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  data: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Compilando el Schema para generar un modelo
+export default mongoose.Model('project', ProjectSchema);
